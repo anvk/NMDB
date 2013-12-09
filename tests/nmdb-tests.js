ee97400d6ff8f7ed1ce2a4bbc2bbbc7169e22b9e
Alexey Novak <alexey.novak.mail@gmail.com>
2013-12-09 16:09:08 -0500

    adding tests to the project. dumb stub for now

---
+++ tests/nmdb-tests.js
@@ -0,0 +1,11 @@
+/*global it, describe, require*/
+
+var chai = require("chai"),
+    expect = chai.expect,
+    nmdb = require("../libs/nmdb");
+
+describe("NMDB tests", function() {
+  it("dummy test", function() {
+    expect(true).to.be.true;
+  });
+});
